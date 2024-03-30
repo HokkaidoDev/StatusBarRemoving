460c7c02930d32ac81a90aee323f2f24513075e1
Magellan-art <rinele8961@glaslack.com>
2024-03-30 21:14:43 +0200

    thirdCommit

--- app/src/main/java/com/hokaido/javarushtestingxml/MainActivity.java
+++ app/src/main/java/com/hokaido/javarushtestingxml/MainActivity.java
@@ -1,40 +1,50 @@
 package com.hokaido.javarushtestingxml;
 
-import android.app.ActionBar;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
+
         super.onCreate(savedInstanceState);
 
-        View decorView = getWindow().getDecorView();
-// Hide the status bar.
-        int uiOptions = decorView.getSystemUiVisibility();
-        uiOptions |=  View.SYSTEM_UI_FLAG_FULLSCREEN;
-        uiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
-
+        setFullScreen();
         setContentView(R.layout.happy_birthday);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets; });
+    }
+
+    private void setFullScreen() {
+        // Hide the status bar and navigation bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+//    Коли конфігурація змінюється (наприклад, при обертанні пристрою), рядок стану може стати видимим.
+//    Щоб впоратися з цим, вам слід повторно застосувати режим занурення(immersive mode):
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            setFullScreen();
+        }
     }
 }
-// І іще цікаво, як зробити так щоб після згортання navigation bar далі міг бути прихованим і з'являвся
-// тільки по кліку по ньому. А то після згортання він вісить перманетно і не зникає більше.
+//* View.SYSTEM_UI_FLAG_FULLSCREEN // сховати status bar, можна і без нього бо це саме є в items в themes
+//* View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //Сховати навігаційну панель.
+//* View.SYSTEM_UI_FLAG_LAYOUT_STABLE // Зберігає стабільний розмір UI, незалежно від того, чи з'являється
+//  системна панель.
+//* View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Дозволяє вашому UI розтягуватися на весь екран, включаючи
+//  область, яку зазвичай займає статус-бар.
+//* View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // Коли цей флаг встановлено, система вирівнює ваше активіти
+//  так, щоб воно розташовувалося під навігаційною панеллю. Це особливо корисно для додатків, які вимагають
+//  повноекранного режиму та використовують жестову навігацію.
+//* View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY; // У повноекранному режимі системна панель може бути знову
+//  відображена, якщо користувач зробить жест знизу вгору. Цей флаг забезпечує, що системна панель буде
+//  схована знову після того, як користувач закінчить взаємодію з нею.
